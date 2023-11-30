082081476f8d0840966904a0b815398efc67e55a
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-11-30 11:41:33 +0200

    Atualizar o main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
-console.log()
+// Dados a serem enviados no corpo da requisição
+const dataToSend = {
+  name: 'string',
+  password: 'string'
+};
+
+// Configurações da requisição POST
+const requestOptions = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json' // Especifica o tipo de conteúdo enviado (JSON neste caso)
+  },
+  body: JSON.stringify(dataToSend) // Converte os dados para formato JSON
+};
+
+// Fazendo a requisição POST usando fetch
+fetch('http://23.20.239.207:5000/api/auth/register', requestOptions)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Erro ao fazer requisição: ' + response.status);
+    }
+    document.write(response.json());
+    return response.json();
+  })
+  .then(data => {
+    console.log(data); // Aqui você pode manipular a resposta recebida
+  })
+  .catch(error => {
+    console.error('Erro:', error);
+  });
