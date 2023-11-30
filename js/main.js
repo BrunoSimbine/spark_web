4e71c66c8ab9703f8d31134c6923894a1ed1eddf
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-11-30 13:39:39 +0200

    Atualizar o main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,32 @@
-fetch('https://bible-api.com/john%203:16')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Erro ao fazer requisição: ' + response.status);
-      }
-      return response.json();
-    })
-    .then(data => {
-      alert('Resposta da requisição: ' + JSON.stringify(data));
-      
-      // Aqui você pode manipular a resposta recebida após a requisição POST ser concluída
-    })
-    .catch(error => {
-      console.error('Erro:', error);
-    });
+function getLoginToken()
+{
+    const dataToSend = {
+        phone: 'string',
+        password: 'string'
+    };
+    
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dataToSend)
+    };
+
+    fetch('http://23.20.239.207:5000/api/auth/login', requestOptions)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Erro ao fazer requisição: ' + response.status);
+          }
+          return response.json();
+        })
+        .then(data => {
+          return JSON.stringify(data);
+          // Aqui você pode manipular a resposta recebida após a requisição POST ser concluída
+        })
+        .catch(error => {
+          console.error('Erro:', error);
+        });
+}
+
+alert(getLoginToken());
