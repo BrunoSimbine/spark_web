1e483bc2973cf0a52bfb5b100937dd58f1f62cb1
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-12-01 13:37:52 +0200

    Atualizar o main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
 
 function sayHello()
 {
-  alert("Hello");
+  
+  fetch('http://23.20.239.207:5000/api/Transaction/get?tokenId=08dbf020-cdd7-47dd-8df5-5fe7755c96b8', {
+  method: 'GET',
+  headers: {
+    'accept': 'text/plain',
+    'Authorization': 'bearer eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJ1c2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiMDhkYmYwMjAtOTdiYy00NjhhLThhOTktMDVmZTk3NDczY2ZkIiwiZXhwIjoxNzAxNTA0NTAwfQ.stDRkHvgfT007VQkpQjAkYUABi9WYy4vjUXRJfq0VXBig9qp6EORcwLccevLl0KQlenJVjKSYKWvxSJu-htTzw'
+  }
+})
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Erro na requisição');
+    }
+    return response.text(); // Extrair a resposta como texto
+  })
+  .then(data => {
+    // Manipular os dados recebidos da resposta
+    console.log('Dados recebidos:', data);
+  })
+  .catch(error => {
+    // Tratamento de erros
+    console.error('Ocorreu um erro:', error);
+  });
+
+  
 }
 
 
