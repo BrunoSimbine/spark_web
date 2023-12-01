a95b9f519d1a99c2260307afcce453429ab2d4c1
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-12-01 15:18:27 +0200

    Atualizar o login.js

--- js/login.js
+++ js/login.js
@@ -1,3 +1,19 @@
+document.getElementById('loginForm').addEventListener('submit', async function(event) {
+      // Impedir o comportamento padrão do formulário
+      event.preventDefault();
+
+      // Aqui você pode adicionar seu próprio código para manipular os dados do formulário
+      // Por exemplo, você pode pegar os valores do formulário e executar alguma ação com eles
+      // Exemplo:
+      await getLogin();
+      window.location.href = 'http://23.20.239.207/';
+      
+      // Você pode fazer outras operações desejadas com os dados do formulário
+
+      // Não se esqueça de adicionar a lógica necessária para processar os dados do formulário
+});
+
+
 async function getLogin()
 {
     const telefone = document.getElementById('inputPhone').value;
