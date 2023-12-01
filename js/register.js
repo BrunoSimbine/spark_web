a7f81a47d18ff3451dfc2cfb56d2913266bb6026
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-12-01 14:46:45 +0200

    Atualizar o register.js

--- js/register.js
+++ js/register.js
@@ -1,8 +1,29 @@
-function createUser()
+async function createUser()
 {
     const nome = document.getElementById('inputName').value;
     const sobrenome = document.getElementById('inputSurname').value;
     const telefone = document.getElementById('inputPhone').value;
     const senha = document.getElementById('inputPassword').value;
+    
+    var result = await fetch('http://23.20.239.207:5000/api/Auth/register', {
+        method: 'POST',
+        headers: {
+          'accept': 'text/plain',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          "phone": telefone,
+          "name": nome,
+          "surname": sobrenome,
+          "password": senha
+        })
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Erro na requisição');
+          }
+          return response.text(); // Extrair a resposta como texto
+        })
+    return result;
 }
 
