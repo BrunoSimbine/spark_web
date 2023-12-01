fc3848c3461b2fd8ebd02efea88e644d43f07e27
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-12-01 14:44:31 +0200

    Atualizar o register.js

--- js/register.js
+++ js/register.js
@@ -1,4 +1,8 @@
-const nome = document.getElementById('inputName').value;
-const sobrenome = document.getElementById('inputSurname').value;
-const telefone = document.getElementById('inputPhone').value;
-const senha = document.getElementById('inputPassword').value;
+function createUser()
+{
+    const nome = document.getElementById('inputName').value;
+    const sobrenome = document.getElementById('inputSurname').value;
+    const telefone = document.getElementById('inputPhone').value;
+    const senha = document.getElementById('inputPassword').value;
+}
+
