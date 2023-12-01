dac1bbf122e16403a54e95f515bd5440f19b0889
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-12-01 15:14:34 +0200

    Atualizar o login.js

--- js/login.js
+++ js/login.js
@@ -1,5 +1,27 @@
-function getLogin()
+async function getLogin()
 {
     const telefone = document.getElementById('inputPhone').value;
     const senha = document.getElementById('inputPassword').value;
+    
+    var token = await fetch('http://23.20.239.207:5000/api/Auth/login', {
+    method: 'POST',
+    headers: {
+      'accept': '*/*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "phone": telefone,
+      "password": senha
+    })
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Erro na requisição');
+      }
+      return response.json(); // Extrair a resposta como JSON
+    }).then(data => {
+        // Armazenando dados
+        localStorage.setItem('token', token.token);
+        
+    })
 }
