94903294175ae489a1d0bb0e3f28cc26ae4ce51a
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-12-01 13:33:16 +0200

    Atualizar o main.js

--- js/main.js
+++ js/main.js
@@ -5,25 +5,14 @@
 function getTokens()
 {
   
-  var tokens = await fetch('http://23.20.239.207:5000/api/Token/get', {
+  var response = await fetch('http://23.20.239.207:5000/api/Token/get', {
   method: 'GET',
   headers: {
     'accept': 'text/plain',
     'Authorization': 'bearer eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJ1c2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiMDhkYmYwMjAtOTdiYy00NjhhLThhOTktMDVmZTk3NDczY2ZkIiwiZXhwIjoxNzAxNTE0NjQwfQ.FaY9XhUB3MfghI74WU0VojPeBN98X1m9jup4wT4cS6cY06IXs9hwl5aytNonW4PZKFZNPeHXVUJPZs1OWW8NLA' 
-    
-})
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Erro na requisição');
-    }
-    return response.json(); // Extrair a resposta como texto
-  })
-  .catch(error => {
-    // Tratamento de erros
-    console.error('Ocorreu um erro:', error);
-  });
+});
   
-  return tokens;
+  return response;
 }
 
 
