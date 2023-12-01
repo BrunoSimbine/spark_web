6b0bfc8e7d83e539de56c246cb482b3d94bc0281
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-12-01 12:02:35 +0200

    Atualizar o main.js

--- js/main.js
+++ js/main.js
@@ -4,10 +4,6 @@
 function getTokens()
 {
   
-  var tokenList = document.getElementById("");
-  
-  
-  
   fetch('http://23.20.239.207:5000/api/Token/get', {
   method: 'GET',
   headers: {
@@ -30,4 +26,35 @@ function getTokens()
     console.error('Ocorreu um erro:', error);
   });
   
+}
+
+function preencherLista() {
+      const dados = [
+        { name: 'Emola', account: '860****59' },
+        { name: 'Mpesa', account: '860****59' },
+        { name: 'BIM', account: '860****59' }
+        // Adicione mais dados conforme necessário
+      ];
+
+      const lista = document.getElementById('minhaLista');
+
+      dados.forEach(item => {
+        const listItem = document.createElement('li');
+        listItem.classList.add('list-group-item', 'list-group-item-action', 'd-flex', 'justify-content-between', 'align-items-start');
+
+        const divConteudo = document.createElement('div');
+        divConteudo.classList.add('ms-2', 'me-auto');
+
+        const divNome = document.createElement('div');
+        divNome.classList.add('fw-bold');
+        divNome.textContent = item.name;
+
+        const account = document.createTextNode(item.account);
+
+        divConteudo.appendChild(divNome);
+        divConteudo.appendChild(account);
+
+        listItem.appendChild(divConteudo);
+        lista.appendChild(listItem);
+      });
 }
