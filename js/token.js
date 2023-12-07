0dabaa59c75947fbec929b7d369353a041e2c83e
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-12-07 10:38:53 +0200

    Atualizar o token.js

--- js/token.js
+++ js/token.js
@@ -12,6 +12,7 @@ document.getElementById('transactionForm').addEventListener('submit', async func
       
     var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
       var wrapper = document.createElement('div')
+    alert(1)
       alertPlaceholder.innerHTML = ''
       wrapper.innerHTML = [
         `<div class="alert alert-${type} alert-dismissible" role="alert">`,
@@ -19,9 +20,10 @@ document.getElementById('transactionForm').addEventListener('submit', async func
         '   <button type="button" class="btn-close"></button>',
         '</div>'
       ].join('')
+    alert(2)
       alertPlaceholder.append(wrapper)
     
-    
+    alert(3)
     
       spinner.style.display = 'none';
       
