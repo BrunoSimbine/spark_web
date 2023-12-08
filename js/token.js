51c71645abb2f53b723d66371be693378ee1552f
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-12-08 16:15:33 +0200

    Atualizar o token.js

--- js/token.js
+++ js/token.js
@@ -10,6 +10,45 @@ document.getElementById('transactionForm').addEventListener('submit', async func
       // Por exemplo, você pode pegar os valores do formulário e executar alguma ação com eles
       // Exemplo:
       
+    
+    
+    
+    const url = 'http://3.94.197.194:5000/api/Transaction/create';
+    const token = 'eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJ1c2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiMDhkYmY3ZjQtOGE1MC00ODU5LTg2MzAtYjFmYWEwMzEyY2ZlIiwiZXhwIjoxNzAyMTMxMDc4fQ.xdP0editlYtRsYGQrzU1GIwrF2xMCFBBsFVl-1nEybYj4rzkrmpK_nmQpKH5XmZunOt4mqztqSRlNKo9p6432w';
+    
+    const data = {
+      amount: 7500,
+      contact: 'string',
+      tokenId: '08dbf7f4-9df0-4ce1-8db7-0b4559f1e9b3',
+      client: 'string'
+    };
+    
+    await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'accept': 'text/plain',
+        'Authorization': `bearer ${token}`
+      },
+      body: JSON.stringify(data)
+    }).then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.text(); // Change this based on what response format you expect
+      })
+      .then(data => {
+        // Handle the data from the response
+        alert(data);
+      })
+      .catch(error => {
+        // Handle errors here
+        console.error('There was a problem with the fetch operation:', error);
+      });
+
+    
+    
+    
     var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
       var wrapper = document.createElement('div')
       alertPlaceholder.innerHTML = ''
