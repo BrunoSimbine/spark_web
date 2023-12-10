23533d305d50e1673482d14c76fb83b1f22f7b4a
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-12-10 10:28:23 +0200

    Atualizar o transactions.js

--- js/transactions.js
+++ js/transactions.js
@@ -1,3 +1,8 @@
+async function fillTable() {
+
+  // Call the function to fill the table
+
+
 const url = 'http://3.94.197.194:5000/api/Transaction/get?tokenId=08dbf947-7daa-4bf4-8918-2d4e92d34bae';
 
 const options = {
@@ -8,18 +13,45 @@ const options = {
   }
 };
 
-fetch(url, options)
+await fetch(url, options)
   .then(response => {
     if (!response.ok) {
       throw new Error('Network response was not ok.');
     }
-    return response.text(); // Change to .json() if the response is JSON
+    return response.json(); // Change to .json() if the response is JSON
   })
   .then(data => {
     // Handle the data received from the API
-    console.log(data);
+
+
+
+  // Function to fill the table with transaction data
+  
+    const tbody = document.getElementById("transactionBody");
+
+    data.forEach(transaction => {
+      const row = document.createElement("tr");
+      
+      // Fill in data for each column
+      row.innerHTML = `
+        <th scope="row">${transaction.number}</th>
+        <td>${transaction.client}</td>
+        <td><span class="badge rounded-pill text-bg-primary">M-Pesa</span></td>
+        <td>${transaction.amount}</td>
+        <td>${transaction.created}</td>
+        <td>${transaction.paid}</td>
+        <td>${transaction.finished}</td>
+        <td>${transaction.contact}</td>
+        <td><span class="badge rounded-pill text-bg-primary">${transaction.status}</span></td>
+        <td><button class="btn btn-danger btn-sm">X</button></td>
+      `;
+      
+      // Append the row to the table body
+      tbody.appendChild(row);
+    });
   })
   .catch(error => {
     // Handle errors during the request
     console.error('There was a problem with the fetch operation:', error);
   });
+}
