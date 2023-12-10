ca7ff3126f6ebcfb7443cd63996f3cf7d29643f6
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-12-10 12:32:00 +0200

    Atualizar o transactions.js

--- js/transactions.js
+++ js/transactions.js
@@ -56,9 +56,9 @@ await fetch(url, options)
         <td>
         
           <div class="dropdown">
-            <button class="btn btn-primary" data-bs-toggle="dropdown" aria-expanded="false">
+            <span data-bs-toggle="dropdown" aria-expanded="false">
               <i class="bi bi-three-dots-vertical"></i>
-            </button>
+            </span>
             <ul class="dropdown-menu">
               <li><a class="dropdown-item" href="#">Imprimir</a></li>
               <li><a class="dropdown-item" href="#">Pagar</a></li>
