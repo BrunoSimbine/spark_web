6d6d38ea51581458c2c92559169499b0be84e0fc
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-12-15 10:56:40 +0200

    Atualizar o main.js

--- js/main.js
+++ js/main.js
@@ -39,7 +39,6 @@ async function registerToken()
       })
       .then(data => {
         // Recarregar a página imediatamente
-            alert(data);
             window.location.reload();
 
        // Aqui você pode lidar com a resposta da requisição
