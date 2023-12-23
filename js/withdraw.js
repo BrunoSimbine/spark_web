4ea94126dbbe09b61dffe21adcaeaad8ff6bbbad
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-12-23 13:38:55 +0200

    Atualizar o withdraw.js

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,6 +1,11 @@
 function initWithdraw()
 {
+    // Selecionando o elemento de âncora pelo ID
+    let meuLink = document.getElementById('meuLink');
     
-}
+    // Alterando a URL do link
+    meuLink.href = 'token.html?id=';
+    
+    }
 
 window.onload = initWithdraw;
