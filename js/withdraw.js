9f4ece46ccb723528ca0170b06fa793f5c6f12ba
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-12-26 09:45:39 +0200

    Atualizar o withdraw.js

--- js/withdraw.js
+++ js/withdraw.js
@@ -18,6 +18,41 @@ async function initWithdraw()
     
     alert(idToken);
     
+    
+    
+    
+    
+    
+    await fetch('http://3.94.197.194:5000/api/Token/withdraw?Id=08dc0006-98c0-4d25-80f1-f1d70dd01f15&Bank=Millennium%20BIM', {
+  method: 'POST',
+  headers: {
+    'accept': 'text/plain',
+    'Authorization': 'Bearer eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJ1c2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiMDhkYmZmMTQtMGYzYi00NjUzLTg0OWItZTMxMzA0NTEyNDBkIiwiZXhwIjoxNzAzNjU5NDYyfQ.iisMcvTfHTL6zL6kG_cTD-TLOSox-UJWdlF-uhr60YowH8aDL714pHbZTMBvIT7VVLe-8Y2vX3IADwDXGjQY7w'
+  },
+  body: ''
+})
+.then(response => {
+  if (!response.ok) {
+    throw new Error('Erro ao fazer a solicitação');
+  }
+  return response.text();
+})
+.then(data => {
+  alert(data);
+})
+.catch(error => {
+  console.error('Ocorreu um erro:', error);
+});
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
     const transactions = [
   {
     name: "Bruno Simbine",
