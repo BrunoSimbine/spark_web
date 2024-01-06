15dcf8cf0e321dafa41039a8b73e5ee38c73eff7
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2024-01-06 09:28:53 +0200

    Atualizar o withdraw.js

--- js/withdraw.js
+++ js/withdraw.js
@@ -14,7 +14,7 @@ async function initWithdraw()
     meuLink.href = 'token.html?id=' + idToken;
     
     
-    var authToken = 'bearer ' + localStorage.getItem('token');
+    var authToken = 'Bearer ' + localStorage.getItem('token');
     
     
     
@@ -23,7 +23,7 @@ async function initWithdraw()
   method: 'POST',
   headers: {
     'accept': 'text/plain',
-    'Authorization': 'Bearer eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJ1c2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiMDhkYmZmMTQtMGYzYi00NjUzLTg0OWItZTMxMzA0NTEyNDBkIiwiZXhwIjoxNzAzNjU5NDYyfQ.iisMcvTfHTL6zL6kG_cTD-TLOSox-UJWdlF-uhr60YowH8aDL714pHbZTMBvIT7VVLe-8Y2vX3IADwDXGjQY7w'
+    'Authorization': authToken
   },
   body: ''
 })
